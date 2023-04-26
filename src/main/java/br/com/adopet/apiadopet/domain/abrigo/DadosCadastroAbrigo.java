aeee031cf3095770c52acdd767ce507ee6298b35
Jose Silvio <josesilvio.bs@gmail.com>
2023-04-26 16:53:11 -0300

    Manuntenção no DTO dados Endereco

--- src/main/java/br/com/adopet/apiadopet/domain/abrigo/DadosCadastroAbrigo.java
+++ src/main/java/br/com/adopet/apiadopet/domain/abrigo/DadosCadastroAbrigo.java
@@ -2,12 +2,13 @@ package br.com.adopet.apiadopet.domain.abrigo;
 
 import br.com.adopet.apiadopet.dto.DadosEndereco;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 
 public record DadosCadastroAbrigo(
 		@NotBlank
 		String nome, 
 		
-		@NotBlank
+		@NotEmpty
 		DadosEndereco endereco, 
 		
 		@NotBlank 
--- src/main/java/br/com/adopet/apiadopet/repository/AbrigoRepository.java
+++ src/main/java/br/com/adopet/apiadopet/repository/AbrigoRepository.java
@@ -1,11 +1,12 @@
 package br.com.adopet.apiadopet.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import br.com.adopet.apiadopet.domain.abrigo.Abrigo;
 
 public interface AbrigoRepository extends JpaRepository<Abrigo, Long>{
 
-	Abrigo findByEmail(String subject);
+	UserDetails findByEmail(String subject);
 
 }
